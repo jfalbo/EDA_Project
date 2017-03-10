e213f55b508a86d954bf84b5f6225a511838857c
jfalbo <jake.falbo@gmail.com>
2017-03-10 10:02:58 -0500

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -7,3 +7,10 @@ hs_pwr$Date <- as.Date(hs_pwr$Date, format = ("%d/%m/%Y"))
 review_dates <- subset(hs_pwr, Date >= "2007-02-01" & Date <= "2007-02-02")
  plot1 <- as.numeric(review_dates$Global_active_power)
   
+ #plot the data 
+ hist(plot1, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowats)")
+ 
+ #save the png with required dimensions 
+ png("plot1.png", width = 480, height = 480)
+ 
+ 
--- plot4.R
+++ plot4.R
@@ -1,6 +1,3 @@
-#Plot 3 
-
- 
 hs_pwr <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = ".")
 hs_pwr$Date <- as.Date(hs_pwr$Date, format = ("%d/%m/%Y"))
 
@@ -11,11 +8,25 @@ pwr_data <- subset(hs_pwr, Date >= "2007-02-01" & Date <= "2007-02-02")
 DT <- paste(as.Date(pwr_data$Date), pwr_data$Time)
 pwr_data$DT <- as.POSIXct(DT)
 
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
 
-png("plot3.png", height = 480, width = 480)
+#add plot 1 - in top left 
+plot(pwr_data$Global_active_power ~ pwr_data$DT, type = "l", xlab = "", ylab = "Global Active Power (kilowats)")
+
+#add second plot 
+
+plot(pwr_data$Voltage~pwr_data$DT, type ="l", ylab = "Voltage", xlab = "datetime")
+
+
+#add third plot 
+
 plot(pwr_data$Sub_metering_1 ~ pwr_data$DT, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
 lines(pwr_data$Sub_metering_2 ~ pwr_data$DT, col = "red")
 lines(pwr_data$Sub_metering_3 ~ pwr_data$DT, col = "blue")
-legend("topright", legend = legend_text, col = c("black", "red", "blue"), lty = 1)
+legend("topright", legend = legend_text, col = c("black", "red", "blue"), lty = 1, bty = "n")
 
-dev.off()
+
+#add final plot 
+
+plot(pwr_data$Global_reactive_power~pwr_data$DT, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
